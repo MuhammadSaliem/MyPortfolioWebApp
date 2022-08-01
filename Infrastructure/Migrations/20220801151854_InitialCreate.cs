5abf6c23263d4e0bbc705eb7c3e52ae6dc0f0df2
_Saleem_ <mohamedselim701@gmail.com>
2022-08-02 00:29:31 +0200

    InitialCreate Migration

---
+++ Infrastructure/Migrations/20220801151854_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FullName = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    ProjectName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    FullName = table.Column<string>(nullable: true),
+                    Profile = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Owners_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Owners",
+                columns: new[] { "Id", "AddressId", "Avatar", "FullName", "Profile" },
+                values: new object[] { new Guid("ac487e39-b30f-45ce-b0f8-003550aced09"), null, "Avatar.jpg", "Khalid Essaadani", "Microsoft MVP / .NET Consultant" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_AddressId",
+                table: "Owners",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioItems");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
