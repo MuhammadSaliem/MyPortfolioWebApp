86a7e838173560c09e7056a4b81661c0262f16cf
_Saleem_ <mohamedselim701@gmail.com>
2022-08-02 00:32:58 +0200

    Add IUnitOfWork Interface

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Core.Interfaces
 {
-    internal class IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
     }
 }
