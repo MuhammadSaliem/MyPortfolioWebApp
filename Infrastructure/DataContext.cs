63662e4f65d7c2d6a967c876fc34aa351325dbcf
_Saleem_ <mohamedselim701@gmail.com>
2022-08-01 16:35:26 +0200

    Configer Models and Add Data Seeding

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,30 @@ namespace Infrastructure
 {
     internal class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValue("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id =Guid.NewGuid(),
+                    Avatar = "Avatar.jpg",
+                    FullName = "Khalid Essaadani",
+                    Profile = "Microsoft MVP / .NET Consultant"
+                });
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+
     }
 }
