f4a26959ca7dddd245186076ae4439d3f3d47219
_Saleem_ <mohamedselim701@gmail.com>
2022-08-01 15:09:55 +0200

    Config Startup.cs add services, useStaticFiles, add defualt Route

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -16,6 +16,7 @@ namespace Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +27,16 @@ namespace Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "defualtRoute",
+                    "{controller = Home}/{action = Index}/{id?}"
+                    );
+            });
 
         }
     }
